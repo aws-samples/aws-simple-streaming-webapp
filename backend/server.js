593eb50057837a8ae1fabd9d37e2fd66e6c38942
Osmar Junior <osmarb@amazon.com>
2021-02-12 16:07:46 -0300

    Server 0.9 - ffmpeg ajusts and socket messaging

--- backend/server.js
+++ backend/server.js
@@ -56,36 +56,16 @@ wss.on('connection', (ws, req) => {
     // Launch FFmpeg to handle all appropriate transcoding, muxing, and RTMP.
     // If 'ffmpeg' isn't in your path, specify the full path to the ffmpeg binary.
     const ffmpeg = child_process.spawn('ffmpeg', [
-      // Facebook requires an audio track, so we create a silent one here.
-      // Remove this line, as well as `-shortest`, if you send audio from the browser.
-      '-f', 'lavfi', '-i', 'anullsrc',
-
-      // FFmpeg will read input video from STDIN
-      '-i', '-',
-
-      // Because we're using a generated audio source which never ends,
-      // specify that we'll stop at end of other input.  Remove this line if you
-      // send audio from the browser.
-      '-shortest',
-
-      // If we're encoding H.264 in-browser, we can set the video codec to 'copy'
-      // so that we don't waste any CPU and quality with unnecessary transcoding.
-      // If the browser doesn't support H.264, set the video codec to 'libx264'
-      // or similar to transcode it to H.264 here on the server.
-      '-vcodec', 'copy',
-
-      // AAC audio is required for Facebook Live.  No browser currently supports
-      // encoding AAC, so we must transcode the audio to AAC here on the server.
+     //input
+      '-i', '-', 
+      //video
+      '-vcodec', 'copy', '-preset', 'veryfast', '-tune', 'zerolatency',
+      //audio
       '-acodec', 'aac',
-
-      // FLV is the container format used in conjunction with RTMP
-      '-f', 'flv',
-
-      // The output RTMP URL.
-      // For debugging, you could set this to a filename like 'test.flv', and play
-      // the resulting file with VLC.  Please also read the security considerations
-      // later on in this tutorial.
-      rtmpUrl
+      //Restransmission
+      '-reconnect', '3', '-reconnect_at_eof', '1', '-reconnect_streamed', '3',
+      //wrapping
+      '-f', 'flv', rtmpUrl
     ]);
 
     // If FFmpeg stops for any reason, close the WebSocket connection.
