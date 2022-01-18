4168ab149edc1e5e176fddac12fbb1f7b0433c99
Osmar Junior <osmarb@amazon.com>
2022-01-18 16:15:26 -0300

    Add CloudFront to websocket Container

---
+++ backend/transwrap_local _peer.js
@@ -0,0 +1,128 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+// transwrap.js http version 
+
+/// import block
+const http = require('http');
+//const fs = require('fs');
+const WebSocket = require('ws');
+const express = require('express');
+const callff = require('child_process'); 
+
+/// app socket definition
+const app = express();
+const port = 3004
+const servertype = 'HTTP'
+const home = '/wwww' /// for heath check only, you can also use as standalone server, by moving the frontend folder to the home www
+
+// https certificates // Be sure to generate those before running
+/*
+const newCert =  'cert.pem';
+const newKey = 'key.pem'
+const certicates = {
+  key: fs.readFileSync(newKey),
+  cert: fs.readFileSync(newCert)
+};
+*/
+
+// wrapper server
+const transwraper = http.createServer(app).listen(port, () => {console.log(`Listening on port: ${port} ${servertype}`)})
+
+// websocket creation
+const wsRef = new WebSocket.Server({ server: transwraper});
+
+app.use((req, res, next) => {
+  console.log(`${servertype}:${req.method}:${req.originalUrl}`);
+
+  wsRef.on('connection', (ws, req) => { 
+    console.log("!!!!!! connection")
+  })
+
+  wsRef.on('open', (ws, req) => { 
+    console.log("!!!!!! connection")
+  })
+
+
+  return next();
+
+
+
+});
+
+app.use(express.static(__dirname + home)); 
+
+// Streaming
+wsRef.on('connection', (ws, req) => {  
+  console.log("Loop 1")
+  //ws.send('MSG: Connected to server'); /// Send this message to the app frontend
+  console.log(`Got connection, URL: ${req.url}`)
+    // em testes
+    if (req.url === '/'){
+      //writeHead(200, {'Content-Type': 'text/html'}); 
+      console.log('é o /')
+
+        ws.on('message', function(message) {
+          // Broadcast any received message to all clients
+          console.log('received: %s', message);
+          ws.broadcast(message);
+        });
+      
+      
+      ws.broadcast = function(data) {
+        this.clients.forEach(function(client) {
+          if(client.readyState === WebSocket.OPEN) {
+            client.send(data);
+          }
+        });
+      };
+
+      /// em testes
+
+      
+
+
+
+
+
+    } else {  
+    ws.on('message', (evt) => {
+      console.log('Event', evt);
+      ffmpeg.stdin.write(evt);
+    });
+
+    ws.on('error', (err) => {
+      console.log('ERROR on websocket', err);
+    });
+
+    const rtmpURL = req.url.slice(7)
+    console.log(`URL seted is ${rtmpURL}`)
+
+    ws.on('close', (evt) => {
+      ffmpeg.kill('SIGINT');
+      console.log(`Connection Closed: ${evt}`)    
+    });
+
+    const ffmpeg = callff.spawn(
+      'ffmpeg', 
+      ['-i', '-', '-vcodec', 'copy', '-preset', 'veryfast', '-tune', 'zerolatency', '-acodec', 'aac', '-reconnect', '3', '-reconnect_at_eof', '1', '-reconnect_streamed', '3',        '-f', 'flv', rtmpURL
+    ]);
+     
+    ffmpeg.on('close', (code, signal) => {
+      console.log(`FFMPEG closed, reason ${code} , ${signal}`);
+      ws.send('Closing Socket'); /// message to front end
+      ws.terminate();
+    });
+
+    ffmpeg.stdin.on('error', (e) => {
+      ws.send(e);
+      console.log(`FFMPEG ERROR: ${e}`)    
+      ws.terminate();
+    });
+
+    ffmpeg.stderr.on('data', (data) => {
+      console.log(`FFMPEG MSG: ${data.toString()}`)    
+      ws.send(data.toString());
+    });
+  }
+});
--- frontend/amplify/backend/function/ISSgetServers/src/index.js
+++ frontend/amplify/backend/function/ISSgetServers/src/index.js
@@ -4,10 +4,13 @@ console.log('Loading Event');
 // aws sdk loading
 const AWS = require('aws-sdk')
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const cloudfront = new AWS.CloudFront();
+const jobSettings = require('./cloudFrontTemplate.json');
+
 
 // handler
 exports.handler = async (event) => {
-
+    var distribValues = {Id:"", DomainName:""};
     // dynamoDB params
     var params = {
         TableName: "ISS-task-dns-track-dev",
@@ -17,13 +20,65 @@ exports.handler = async (event) => {
         ExpressionAttributeNames: {
             "#serverPosition": "role"
         },
-        ProjectionExpression: "dns, #serverPosition, replaced"
+        ProjectionExpression: "dns, #serverPosition, replaced, eventid"
     };
     try {
-        // scan get all servers where replaced = n
         var response = await dynamodb.scan(params).promise()
         if (response.Count > 1){
-        console.log("Response", response); 
+            if (response.Items[0].eventid != "apigw"){
+                console.log("CloudFront update")
+                // action 1 create a new distributions adding the server to the distribution
+                jobSettings.DistributionConfig.CallerReference = Date.now().toString()
+                jobSettings.DistributionConfig.Origins.Items[0].Id = response.Items[0].dns
+                jobSettings.DistributionConfig.Origins.Items[0].DomainName = response.Items[0].dns
+                jobSettings.DistributionConfig.DefaultCacheBehavior.TargetOriginId = response.Items[0].dns
+                var CFparams = jobSettings
+                //console.log(jobSettings); 
+                await cloudfront.createDistribution(CFparams, function(err, data) {
+                    if (err) console.log("err", /*err, err.stack*/); // an error occurred
+                    else{
+                        //console.log(data);
+                        distribValues.Id = data.Distribution.Id
+                        distribValues.DomainName = data.Distribution.DomainName
+
+                        console.log(distribValues)
+                    }             
+                }).promise();
+
+                if(distribValues.DomainName){
+                    console.log("Dynamo Update")
+                    let servers = [{"role":"primary","dns":distribValues.DomainName, "distid":distribValues.Id, "eventid":"apigw","replaced":"no"},{"role":"secondary","dns":distribValues.DomainName, "distid":distribValues.Id, "eventid":"apigw","replaced":"no"}]
+                    for (const [index, server] of servers.entries()){
+                        var params = {
+                            TableName: "ISS-task-dns-track-dev",
+                            Item:{
+                                "role": server.role,
+                                "dns": server.dns,
+                                "distid": server.distid,
+                                "eventid": server.eventid,
+                                "replaced": server.replaced
+                            }
+                        }
+
+                        console.log("Params", params)
+                        
+
+                        try {
+                            await dynamodb.put(params, function(err, data) {
+                                if (err) console.log(err, err.stack); // an error occurred
+                                else{
+                                    console.log("Dynamo!!!", data);           // successful response
+                                }
+                            }).promise();
+                            
+                        } catch (err) {
+                            console.log(err)
+                        }
+                    }
+                }
+                response = await dynamodb.scan(params).promise()
+            }
+             
         const resp = {
             statusCode: 200,
             headers: {
@@ -34,44 +89,10 @@ exports.handler = async (event) => {
         };
         return resp
         } else {
-            //if no data, then load some dummy data
-            let servers = [{"role":"primary","dns":"ec2-3-235-3-206.compute-1.amazonaws.com","eventid":"sss","replaced":"no"},{"role":"secondary","dns":"ec2-3-83-82-164.compute-1.amazonaws.com","eventid":"sss","replaced":"no"}]
-            for (const [index, server] of servers.entries()){
-                // dynamoDB params                
-                 var params = {
-                    TableName: "ISS-task-dns-track-dev",
-                    Item:{
-                        "role": server.role,
-                        "dns": server.dns,
-                        "eventid": server.eventid,
-                        "replaced": server.replaced
-                    }
-                }
-                try {
-                    var respPut = await dynamodb.put(params).promise()
-                    console.log("Put! response", respPut);
-                } catch (err) {
-                    console.log(err)
-                    return "ERROR"
-                }
-            }
-            // then scan table
-            //dynamo params
-            var params = {
-                TableName: "ISS-task-dns-track-dev",
-                FilterExpression: "replaced = :n",
-                ExpressionAttributeValues: {
-                    ":n": "no"},
-                ExpressionAttributeNames: {
-                    "#serverPosition": "role"
-                },
-                ProjectionExpression: "dns, #serverPosition, replaced"
-            };
-            try {
-                var response = await dynamodb.scan(params).promise()
-                console.log("Response Dynamo", response)            
+                console.log("Nothing yet in the table")   
+                response = "You need to deploy the backend container before loading this page"        
                 const resp = {
-                    statusCode: 200,
+                    statusCode: 503,
                     headers: {
                         "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
                         "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
@@ -79,14 +100,10 @@ exports.handler = async (event) => {
                     body: JSON.stringify(response)
                 };
                 return resp
-            } catch (err) {
-                console.log(err)
-                return "ERROR"
             }
-        }
-        
     } catch (err) {
         console.log(err)
         return "ERROR"
     }
+            
 };
